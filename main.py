d504e0a6972e185db5f032f2c89c2ebbc41a90b6
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2024-03-14 19:23:18 +0200

    Статичний та динамічний поліморфізм

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-# качина типізація
+# Статичний та динамічний поліморфізм
 
-class Duck:
-    def quack(self):
-        print("Кря!")
+# статичний поліморфізм
 
-
-class Person:
-    def quack(self):
-        print("Крякає по-людськи!")
+def add(x, y):
+    return x + y
 
 
-def make_it_quack(obj):
-    obj.quack()
+result1 = add(1, 4)
+result2 = add("Hello ", "World")
+print(result1, result2)
 
 
-duck = Duck()
-person = Person()
-make_it_quack(duck)
-make_it_quack(person)
+# динамічний поліморфізм
+class Animal:
+    def make_sound(self):
+        pass
+
+
+class Dog(Animal):
+    def make_sound(self):
+        return "Bark!"
+
+
+class Cat(Animal):
+    def make_sound(self):
+        return "Meow!"
+
+
+def animal_speak(animal):
+    return animal.make_sound()
+
+
+dog, cat = Dog(), Cat()
+result1 = animal_speak(dog)
+result2 = animal_speak(cat)
+print(result1, result2)
